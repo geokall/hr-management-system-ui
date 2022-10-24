9743a39bb097625e5df027e471501d3f4d2e0f2f
geokall <gkallimanis@iknowhow.com>
2022-10-25 01:18:27 +0300

    Added required scss class for required fields. Applied this pattern on login/register

--- src/app/features/components/login/login.component.ts
+++ src/app/features/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {environment} from "../../../../environments/environment";
 import {Router} from "@angular/router";
 import {AuthService} from "../../../core/shared/services/auth.service";
@@ -25,8 +25,8 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.loginForm = new FormGroup({
-      username: new FormControl(''),
-      password: new FormControl(''),
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
     });
   }
 
